6737e26d0a2ad4528ba823266832a4d1e59d1a12
Patrick Kabuga <kabugap02@gmail.com>
2023-03-27 00:53:28 +0300

    [feat]: Add Blog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
