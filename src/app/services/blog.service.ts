dd37787f082612c4f89393688c6bed8e12722259
Patrick Kabuga <kabugap02@gmail.com>
2023-03-27 01:42:40 +0300

    [chore]: Add blog logic

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -13,4 +13,15 @@ export class BlogService {
   getBlogs(): Observable<Blog[]> {
     return this.http.get<Blog[]>('assets/data.json');
   }
+
+  getCurrentBlog(id: string): Blog {
+    let allBlogs: Blog[] = [];
+
+    this.getBlogs().subscribe(data => {
+      allBlogs = data;
+    });
+
+    const currentBlog: Blog = allBlogs.find(blog => blog.id === id) || {id, title: '404', img: '404', description: '404', body: '404'};
+    return currentBlog;
+  }
 }
