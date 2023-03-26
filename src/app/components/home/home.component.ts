6d338149a00f2cc46e7b0eae5aec09b4a9e83802
Patrick Kabuga <kabugap02@gmail.com>
2023-03-26 21:39:36 +0300

    [feat]: Render data to template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
+import { Blog } from 'src/app/models/Blog';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  blogs: Blog[] = [];
 
+  constructor (private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogService.getBlogs().subscribe(data => {
+      this.blogs = data;
+    })
+  }
 }
