db39ae4fa47ad8fbfe1f5d26ac8872fde6540136
Patrick Kabuga <kabugap02@gmail.com>
2023-03-26 21:38:28 +0300

    [chore]: get blog data

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Blog } from 'src/app/models/Blog';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBlogs(): Observable<Blog[]> {
+    return this.http.get<Blog[]>('assets/data.json');
+  }
 }
