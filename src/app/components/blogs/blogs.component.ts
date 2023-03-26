18b97c49fabc5db7572396e3470b018c4c082e22
Patrick Kabuga <kabugap02@gmail.com>
2023-03-27 01:43:08 +0300

    [chore]: Render blog to template

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from 'src/app/services/blog.service';
+import { Blog } from 'src/app/models/Blog';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css']
 })
-export class BlogsComponent {
 
+export class BlogsComponent implements OnInit {
+  id: string;
+  currentBlog: Blog;
+
+  constructor(private route: ActivatedRoute, private blogService: BlogService) { 
+    this.id = this.route.snapshot.paramMap.get('id') || '1';
+    this.currentBlog = this.blogService.getCurrentBlog(this.id);
+  }
+
+  ngOnInit(): void {}
 }
+
+
