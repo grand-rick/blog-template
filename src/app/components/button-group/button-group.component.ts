aee8dc5494f137d22434f38df940bb401ac8aaec
Patrick Kabuga <kabugap02@gmail.com>
2023-03-28 18:39:06 +0300

    [chore]: Remove button group

--- src/app/components/button-group/button-group.component.ts
+++ src/app/components/button-group/button-group.component.ts
@@ -9,20 +9,21 @@ export class ButtonGroupComponent {
   activeButton: string = 'button1';
 
   getLineStyle(): string {
-    const buttonElements = document.querySelectorAll('.button');
-    const activeButtonIndex = Array.from(buttonElements).findIndex(button => button.classList.contains('active'));
+    // const buttonElements = document.querySelectorAll('.button');
+    // const activeButtonIndex = Array.from(buttonElements).findIndex(button => button.classList.contains('active'));
 
-    if (activeButtonIndex === -1) {
-      return 'translateX(-100%)';
-    }
+    // if (activeButtonIndex === -1) {
+    //   return 'translateX(-100%)';
+    // }
 
-    const activeButtonElement = buttonElements[activeButtonIndex];
-    const activeButtonRect = activeButtonElement.getBoundingClientRect();
-    const buttonGroupRect = activeButtonElement.parentElement?.getBoundingClientRect();
+    // const activeButtonElement = buttonElements[activeButtonIndex];
+    // const activeButtonRect = activeButtonElement.getBoundingClientRect();
+    // const buttonGroupRect = activeButtonElement.parentElement?.getBoundingClientRect();
 
     // const translateX = activeButtonRect.left - buttonGroupRect.left;
     // const scaleX = activeButtonRect.width / buttonGroupRect.width;
 
-    return `translateX(${translateX}px) scaleX(${scaleX})`;
+    // return `translateX(${translateX}px) scaleX(${scaleX})`;
+    return '404'
   }
 }
