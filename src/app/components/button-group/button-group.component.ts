29c6677e87eb9c2caa09dfd64d6c7890f3d57a3e
Patrick Kabuga <kabugap02@gmail.com>
2023-03-29 14:01:55 +0300

    [chore]: Add button-category logic

--- src/app/components/button-group/button-group.component.ts
+++ src/app/components/button-group/button-group.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
+import { Blog } from 'src/app/models/Blog';
 
 @Component({
   selector: 'app-button-group',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class ButtonGroupComponent {
   activeButton: string = 'button1';
+  allBlogs: Blog[] = [];
+  allCategories: string[] = [];
+
+  @Output() category: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private blogService: BlogService) {
+    this.blogService.getBlogs().subscribe(data => {
+      this.allBlogs = data;
+
+      this.allBlogs.forEach(blog => {
+        if (!this.allCategories.includes(blog.category)) {
+          this.allCategories.push(blog.category);
+        }
+      })
+    })
+  }
 
   getLineStyle(): string {
     // const buttonElements = document.querySelectorAll('.button');
