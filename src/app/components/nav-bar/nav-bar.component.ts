84e6b665c3571405b071f33593e169c57c45b61e
Patrick Kabuga <kabugap02@gmail.com>
2023-04-04 23:15:28 +0300

    [feat]: Add hamburger menu on mobile

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
 
+  ngOnInit(): void {
+    let btn: HTMLElement = this.elementRef.nativeElement.querySelector('#menu-btn');
+    let nav: HTMLElement = this.elementRef.nativeElement.querySelector('#menu');
+
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('open');
+      nav.classList.toggle('flex');
+      nav.classList.toggle('hidden');
+    });
+  }
 }
